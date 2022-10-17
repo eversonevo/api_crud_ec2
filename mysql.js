b4e852f42b24375b57ad01a9a76af138e69710c3
eversonevo <73645835+eversonevo@users.noreply.github.com>
2022-10-17 19:21:39 -0300

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -12,11 +12,11 @@ const mysql = require('mysql2');
 
 var pool = mysql.createPool({
     "connectionLimit": 1000,
-    "user": "u903973099_api_teste_bd",
-    "password": "api_teste_BD_2021",
-    "database": "u903973099_api_teste_bd",
-    "host": "185.211.7.205",
-    "port": 3306,
+    "user": process.env.MYSQL_USER || "root",
+    "password": process.env.MYSQL_PASSWORD || "root",
+    "database": process.env.MYSQL_DATABASE || "ecommerce",
+    "host": process.env.MYSQL_HOST || "localhost",
+    "port": process.env.MYSQL_PORT || 3306,
 });
 exports.execute = (query, params=[])=>{
     return new Promise((resolve, reject)=>{
